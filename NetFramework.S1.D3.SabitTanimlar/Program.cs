0f30dae462febdea3ff323c75facf49b8a4e7b26
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-02-09 22:56:16 +0300

    3-Sabit Tanımlar

---
+++ NetFramework.S1.D3.SabitTanimlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S01.D3.SabitTanimlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 10;
+
+            sayi = 1001;    // sayi değişikeni const olmadığı için hata almayız
+
+            const int ortakBolumDeger = 2;
+
+            int sonuc = sayi / ortakBolumDeger;
+
+            // ortakBolumDeger = 3;    //ortakBolumDeger değişkeni const deger olduğu için hata alırız.
+        }
+    }
+}
