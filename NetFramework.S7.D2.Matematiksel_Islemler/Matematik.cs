efccb0b4ea2cbc41cb194a91a6edb2a33dd030d8
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-03-15 01:35:07 +0300

    Create Matematik.cs

---
+++ NetFramework.S7.D2.Matematiksel_Islemler/Matematik.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S07.D2.Matematiksel_Islemler
+{
+    public class Matematik
+    { 
+        // Topla metodu
+        public decimal toplamaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        // Çıkarma metodu
+        public decimal cikartmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        // Bölme metodu
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        // Çarpma
+        public decimal carpmaIslemi(decimal sayi1, decimal sayi2)
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+
+        // Menü ekranı
+        public void menuHazirla()
+        {
+            Console.Clear();
+            Console.WriteLine("*** Menu ***");
+            Console.WriteLine("************");
+            Console.WriteLine("1 - Toplama");
+            Console.WriteLine("2 - Çıkarma");
+            Console.WriteLine("3 - Çarpma");
+            Console.WriteLine("4 - Bölme");
+            Console.WriteLine("************************");
+            Console.Write("LÜTFEN DEĞER SEÇİNİZ: ");
+        }
+    }
+}
