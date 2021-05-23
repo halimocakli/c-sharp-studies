8bc7133d9ae48c5631364591582e82c65eedfe32
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-23 23:55:28 +0300

    Create MusteriAdres.cs

---
+++ NetFramework.S11.D1.InnerTypeModelleme/MusteriAdres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S11.D1.InnerTypeModelleme
+{
+    public class MusteriAdres
+    {
+        public string adresTip { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string Adres { get; set; }
+
+        public MusteriAdres()
+        {
+            Console.WriteLine("Müşteri adres yapıcı metodu çalıştı...");
+        }
+
+        public void MusteriAdresTestMetot()
+        {
+            Console.WriteLine("MusteriAdresTestMetot");
+        }
+    }
+}
