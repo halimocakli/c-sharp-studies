af5cc03e2cf09cfe1a2000bc82ff20de3294a65e
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-23 23:55:09 +0300

    Create MusteriSiparisBilgisi.cs

---
+++ NetFramework.S11.D1.InnerTypeModelleme/MusteriSiparisBilgisi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S11.D1.InnerTypeModelleme
+{
+    public class MusteriSiparisBilgisi
+    {
+        public string siparisNumarasi { get; set; }
+
+        public Urun[] Urunler;
+
+        public MusteriSiparisBilgisi()
+        {
+            Urunler = new Urun[2];
+        }
+
+        public void MusteriSiparisBilgisiTestMetot()
+        {
+            Console.WriteLine("MusteriSiparisBilgisiTestMetot");
+        }
+    }
+}
