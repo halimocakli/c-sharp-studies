1837095bc977ab06acf890dd652dbe20ccf0153b
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-24 00:37:26 +0300

    Create Test.cs

---
+++ NetFramework.S12.D1.KalitimNedir/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S12.D1.KalitimNedir
+{   
+    public class Test
+    // public class Test : Egitmen
+    {
+        // Egitmen nesnemizi Sealed anahtar sözcüğü ile mühürleyerek türetilmesini engellediğimiz için "Test:Egitmen" deyimi bize hata verir.
+        // Sealed anahtar sözcüğü ile mühürlenen nesneler en fazla başka bir nesnenin içerisinde InnerType olarak kullanılabilir.
+        // Hata aldığımız sınıf bildirimi yorum satırı içerisine alındı.
+    }
+}
