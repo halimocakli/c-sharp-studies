c6a6383b06fa81f6813243630a94d4c8cd50eb07
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-24 00:37:12 +0300

    Create Personel.cs

---
+++ NetFramework.S12.D1.KalitimNedir/Personel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S12.D1.KalitimNedir
+{
+    // TemelTip sınıfını entegre edildi
+    public class Personel : TemelTip
+    {
+        public DateTime IsBaslangicTarihi { get; set; }
+        public DateTime IsBitisTarihi { get; set; }
+        public int IzinGunSayisi { get; set; }
+
+        public Personel()
+        {
+            Console.WriteLine("Personel nesnesi yapıcı metodu çalıştı.");
+        }
+    }
+}
