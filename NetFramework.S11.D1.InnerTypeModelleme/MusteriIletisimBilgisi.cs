b64a8340c042cc892b648796ad0f7de3131b57fc
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-23 23:55:23 +0300

    Create MusteriIletisimBilgisi.cs

---
+++ NetFramework.S11.D1.InnerTypeModelleme/MusteriIletisimBilgisi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S11.D1.InnerTypeModelleme
+{
+    public class MusteriIletisimBilgisi
+    {
+        public int iletisimTip { get; set; }   // 717770001 : Mobil - 717770002 : Sabit Hat
+        public string alanKodu { get; set; }
+        public string numara { get; set; }
+        public bool aktifPasif { get; set; }
+
+        public void MusteriIletisimBilgisiTestMetot()
+        {
+            Console.WriteLine("MusteriIletisimBilgisiTestMetot");
+        }
+    }
+}
