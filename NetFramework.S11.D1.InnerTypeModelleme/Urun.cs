ff5d8207c52103589f0a7f55a3097844f05b11f7
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-23 23:55:03 +0300

    Create Urun.cs

---
+++ NetFramework.S11.D1.InnerTypeModelleme/Urun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S11.D1.InnerTypeModelleme
+{
+    public class Urun
+    {
+        public int urunID { get; set; }
+        public string urunTanim { get; set; }
+        public decimal urunFiyat { get; set; }
+
+        public Urun()
+        {
+            Console.WriteLine("Urun yapıcı metodu çalıştı...");
+        }
+
+        public void UrunTestMetot()
+        {
+            Console.WriteLine("UrunTestMetot");
+        }
+    }
+}
