695b9958d5ac2bbaf853a82333ce8cf3cbc4f016
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-28 17:31:28 +0300

    Create Televizyon.cs

---
+++ NetFramework.S12.D2.KalitimUygulama/Televizyon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S12.D2.KalitimOdevUygulamasi
+{
+    public sealed class Televizyon : Urun
+    {
+        public bool SmartTV { get; set; }
+        public bool HDMI { get; set; }
+        public string EkranBoyutu { get; set; }
+    }
+}
