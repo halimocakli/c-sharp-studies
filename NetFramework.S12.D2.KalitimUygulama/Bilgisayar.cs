693f38ad19ac4c8bc00f6cb2098a133d51a86a8f
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-28 17:31:15 +0300

    Create Bilgisayar.cs

---
+++ NetFramework.S12.D2.KalitimUygulama/Bilgisayar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S12.D2.KalitimOdevUygulamasi
+{
+    public sealed class Bilgisayar : Urun
+    {
+        public string Islemci { get; set; }
+        public string Ram { get; set; }
+        public string EkranKarti { get; set; }
+    }
+}
