e092d146bcaee7e597a607f025d3574c6d5b382b
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-05-31 03:01:36 +0300

    Create Dortgen.cs

---
+++ NetFramework.S13.D1.SanalMetot/Dortgen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S13.D1.SanalMetotNedir
+{
+    public class Dortgen : Sekil
+    {
+        public Dortgen(int boy, int en) : base(boy, en)
+        {
+        }
+
+        public override double Alan()
+        {
+            return En * Boy;
+        }
+    }
+}
