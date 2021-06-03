2fa6b2a77d9fd17d983b505ddd8fc2626dbebcc0
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-06-03 21:51:40 +0300

    Create Koyun.cs

---
+++ NetFramework.S13.D1.SanalMetot/Koyun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S13.D1.SanalMetotNedir
+{
+    class Koyun : Memeli
+    {
+        public string Turu;
+
+        public Koyun(string turu, int boy, int agirlik) : base(boy, agirlik)
+        {
+            this.Turu = turu;
+        }
+
+        public override void Konus()
+        {
+            Console.WriteLine("Ben bir koyunum!");
+        }
+    }
+}
