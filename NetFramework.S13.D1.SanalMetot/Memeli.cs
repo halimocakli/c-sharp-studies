9ae9781f65d5bb13d5c299f49f9fbf63a87db044
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-06-04 16:19:43 +0300

    Create Memeli.cs

---
+++ NetFramework.S13.D1.SanalMetot/Memeli.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S13.D1.SanalMetotNedir
+{
+    public class Memeli
+    {
+        public double Boy;
+        public double Agirlik;
+
+        public Memeli(double boy, double agirlik)
+        {
+            this.Boy = boy;
+            this.Agirlik = agirlik;
+        }
+
+        public virtual void Konus()
+        {
+            Console.WriteLine("Ben konusamam");
+        }
+    }
+}
