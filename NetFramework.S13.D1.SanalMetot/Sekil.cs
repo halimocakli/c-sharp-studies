f87e2588a3484ebc3f679092fa7b12909b0b40fa
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-06-08 21:52:04 +0300

    Create Sekil.cs

---
+++ NetFramework.S13.D1.SanalMetot/Sekil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S13.D1.SanalMetotNedir
+{
+    public class Sekil
+    {
+        public double Boy;
+        public double En;
+
+        public Sekil(double boy, double en)
+        {
+            this.Boy = boy;
+            this.En = en;
+        }
+
+        public virtual double Alan()
+        {
+            return 0;
+
+        }
+    }
+}
