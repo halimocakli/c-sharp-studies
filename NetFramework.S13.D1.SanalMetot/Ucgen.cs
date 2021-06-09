027b6a52fae2bc5152b33a4685f3c6da73f86de2
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-06-09 22:08:28 +0300

    51-Sanal Metot

---
+++ NetFramework.S13.D1.SanalMetot/Ucgen.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S13.D1.SanalMetotNedir
+{
+    public class Ucgen : Sekil
+    {
+        public Ucgen(int boy, int en) : base(boy, en)
+        {
+        }
+
+        public override double Alan()
+        {
+            return (En * Boy) / 2;
+        }
+    }
+}
