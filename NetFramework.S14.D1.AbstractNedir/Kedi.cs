da254e8301ca0aed232b4676e0c990bc82f8ecfd
halimocakli <halimcanocakli@gmail.com>
2021-08-07 21:54:00 +0300

    Create Kedi.cs

---
+++ NetFramework.S14.D1.AbstractNedir/Kedi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S14.D1.AbstractNedir
+{
+    class Kedi : Memeli
+    {
+        private string Turu;
+
+        public Kedi(string turu, double en, double boy) : base(en, boy)
+        {
+            this.Turu = turu;
+        }
+    }
+}
