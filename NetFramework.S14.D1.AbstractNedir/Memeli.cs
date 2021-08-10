22aeecd1f30269a80a057d069ad85161079c0f16
halimocakli <halimcanocakli@gmail.com>
2021-08-10 08:57:42 +0300

    Create Memeli.cs

---
+++ NetFramework.S14.D1.AbstractNedir/Memeli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S14.D1.AbstractNedir
+{
+    abstract class Memeli
+    {
+        public double en;
+        public double boy;
+
+        public Memeli(double en, double boy)
+        {
+            this.en = en;
+            this.boy = boy;
+        }
+    }
+}
