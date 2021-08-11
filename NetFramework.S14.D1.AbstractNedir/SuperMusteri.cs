4f1603b1b00c562f001907c00e67a1e067d6cb73
halimocakli <halimcanocakli@gmail.com>
2021-08-11 08:34:23 +0300

    53-Abstract Nedir?

---
+++ NetFramework.S14.D1.AbstractNedir/SuperMusteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S14.D1.AbstractNedir
+{
+    public class SuperMusteri : Musteri
+    {
+
+    }
+}
