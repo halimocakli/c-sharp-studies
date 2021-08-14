b727cbf7c11e88804f88ab622dad2e29e241cbf5
halimocakli <halimcanocakli@gmail.com>
2021-08-14 08:51:00 +0300

    Create Imusteri.cs

---
+++ NetFramework.S15.D1.InterfaceNedir/Imusteri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D1.InterfaceNedir
+{
+    public interface Imusteri
+    {
+        // Field'lar
+
+        int Id { get; set; }
+        string isim { get; set; }
+        string soyIsim { get; set; }
+
+        // Metotlar
+
+        int YeniKayit(string isim, string soyisim);
+        int KayitDuzenle(int id, string isim, string soyisim);
+        int KayitSil(int id);
+    }
+}
