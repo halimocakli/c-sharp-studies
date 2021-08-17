eeab774f414f0d8cea946f361cca795ba3442d6f
halimocakli <halimcanocakli@gmail.com>
2021-08-17 12:51:24 +0300

    Create CourseProductModel.cs

---
+++ NetFramework.S15.D2.InterfaceApplication/CourseProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D2.InterfaceApplication
+{
+    public class CourseProductModel : IProductModel
+    {
+        public string UrunAdi { get; set; }
+        public bool SiparisGerceklesmeKontrol { get; private set; }
+        public void UrunSevk(CustomerModel musteri)
+        {
+            if (SiparisGerceklesmeKontrol == false)
+            {
+                Console.WriteLine($"{UrunAdi} eğitimi {musteri.Isim} isimli müşteri hesabına eklendi.");
+                SiparisGerceklesmeKontrol = true;
+            }
+        }
+    }
+}
