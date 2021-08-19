6460487af2e5f3cfaa31f2d6f27e6dbd7f70a037
halimocakli <halimcanocakli@gmail.com>
2021-08-19 16:55:01 +0300

    Create CustomerModel.cs

---
+++ NetFramework.S15.D2.InterfaceApplication/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D2.InterfaceApplication
+{
+    public class CustomerModel : ICustomerModel
+    {
+        public string Isim { get; set; }
+        public string SoyIsim { get; set; }
+        public string Adres { get; set; }
+        public string Sehir { get; set; }
+        public string EmailAdres { get; set; }
+        public string TelefonNumarasi { get; set; }
+    }
+}
