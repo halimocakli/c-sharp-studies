d7f3b56a4d7185bfebd8346cb73a6e0c2f7809f4
halimocakli <halimcanocakli@gmail.com>
2021-08-20 15:38:10 +0300

    Create DigitalProduct.cs

---
+++ NetFramework.S15.D2.InterfaceApplication/DigitalProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D2.InterfaceApplication
+{
+    public class DigitalProduct : IDigitalProductModel
+    {
+        // Bu sınıf bir dijital ürün sınıfıdır. Örnek olarak: Film, Yazılım, Dijital Gazete vs.
+        // Dolayısıyla bu ürünü diğer ürünler gibi sevk edemeyiz ya da adrese gönderemeyiz.
+        // Sonuç olarak dijital ürünü ancak e-mail ile gönderebiliriz.
+
+        public string UrunAdi { get; set; }
+
+        public bool SiparisGerceklesmeKontrol { get; private set; }
+
+        public int GeriyeKalanIndirmeler { get; private set; } = 5;
+
+        public void UrunSevk(CustomerModel musteri)
+        {
+            if (SiparisGerceklesmeKontrol == false)
+            {
+                Console.WriteLine($"{ UrunAdi } siparişinin mail ile gönderilme işlemi { musteri.Isim } adlı müşteri icin { musteri.EmailAdres } adresi hedeflenerek başlatılıyor");
+                GeriyeKalanIndirmeler--;
+                if (GeriyeKalanIndirmeler < 1)
+                {
+                    SiparisGerceklesmeKontrol = true;
+                    GeriyeKalanIndirmeler = 0;
+                }
+            }
+        }
+    }
+}
