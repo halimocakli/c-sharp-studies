741ab5eb442677c29648e358cc47c3632343abdc
halimocakli <halimcanocakli@gmail.com>
2021-08-23 10:42:26 +0300

    Create IDigitalProductModel.cs

---
+++ NetFramework.S15.D2.InterfaceApplication/IDigitalProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D2.InterfaceApplication
+{
+    public interface IDigitalProductModel : IProductModel
+    {
+        /*
+         * Görüldüğü gibi IDigitalProductModel Interface'ine IProductModel Interface'i implement edilebiliyor.
+         * Bu durum daha sağlıklı bir kod oluşmasını sağlar çünkü dikkat ederseniz Program Class'ı dosyası içerisinde bulunan OrnekVeriEkle() metodu
+         * içerisinde DigitalProduct türünden bir itemi IProductModel Interface'i türünden elemanlar tutan listeye ekliyoruz. IProductModel
+         * Interface'ini IDigitalProduct Interface'ine implement ettiğimiz için bir problem çıkmayacaktır. Class Inheritence gibi düşünebiliriz.
+         */
+
+        // Program Class'ı içerisinde bulunan foreach bloğu içindeki if bloğuna giderek notları okursanız daha iyi anlarsınız
+        int GeriyeKalanIndirmeler { get; }
+    }
+}
