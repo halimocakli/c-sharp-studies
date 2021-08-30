80f16f5a74ba784c30c4234a595b647779a5419b
halimocakli <halimcanocakli@gmail.com>
2021-08-30 16:36:52 +0300

    Create IProductModel.cs

---
+++ NetFramework.S15.D2.InterfaceApplication/IProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D2.InterfaceApplication
+{
+    public interface IProductModel
+    {
+        string UrunAdi { get; set; }
+
+        bool SiparisGerceklesmeKontrol { get; }
+
+        void UrunSevk(CustomerModel iCustomer);
+    }
+}
