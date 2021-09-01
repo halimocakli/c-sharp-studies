920e08bf5822b5a2e63eba5a400807e3fd7196a9
halimocakli <halimcanocakli@gmail.com>
2021-09-01 09:28:18 +0300

    Create PhysicalProductModel.cs

---
+++ NetFramework.S15.D2.InterfaceApplication/PhysicalProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S15.D2.InterfaceApplication
+{
+    public class PhysicalProductModel : PhysicalProductBaseClass, IProductModel
+    {
+        // Bu sınıf bir fizksel ürün sınıfıdır. Dolayısıyla ürünleri kargo ile göndermemiz gerekiyor.
+        public string UrunAdi { get; set; }
+        public bool SiparisGerceklesmeKontrol { get; private set; }
+
+        public void UrunSevk(CustomerModel musteri)
+        {
+            if (SiparisGerceklesmeKontrol == false)
+            {
+                Console.WriteLine($"{ UrunAdi } siparişinin sevk simülesyonu { musteri.Isim } adlı müşteri icin { musteri.Sehir } konumu hedeflenerek başlatılıyor");
+                SiparisGerceklesmeKontrol = true;
+            }
+        }
+    }
+}
