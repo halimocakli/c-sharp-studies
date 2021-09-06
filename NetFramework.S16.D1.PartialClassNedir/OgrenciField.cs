e06776abd789e9c516415f388d7980025e6b0444
halimocakli <halimcanocakli@gmail.com>
2021-09-06 10:50:24 +0300

    Create OgrenciField.cs

---
+++ NetFramework.S16.D1.PartialClassNedir/OgrenciField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S16.D1.PartialClassNedir
+{
+    public partial class Ogrenci
+    {
+        public int ogrenciID { get; set; }
+        public string ogrenciIsim { get; set; }
+        public string ogrenciSoyIsim { get; set; }
+        public string ogrenciNumarasi { get; set; }
+    }
+}
