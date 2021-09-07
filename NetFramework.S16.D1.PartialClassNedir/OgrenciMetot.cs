dc10af8eb0d23faf0b7c264ea3051736298ef5c5
halimocakli <halimcanocakli@gmail.com>
2021-09-07 10:21:16 +0300

    Create OgrenciMetot.cs

---
+++ NetFramework.S16.D1.PartialClassNedir/OgrenciMetot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S16.D1.PartialClassNedir
+{
+    public partial class Ogrenci
+    {
+        public int YeniKayitOgrenci(Ogrenci o)
+        {
+            Console.WriteLine("Öğrenci kayıt işlemi başarılı");
+            return 1;
+        }
+
+        public int KayitGuncelleOgrenci(Ogrenci o)
+        {
+            Console.WriteLine("Oğrenci kaydı güncellendi");
+            return 1;
+        }
+
+        public int KayitSilOgrenci(int id)
+        {
+            Console.WriteLine("Öğrenci kaydı silindi");
+            return 1;
+        }
+    }
+}
