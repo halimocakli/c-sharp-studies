01bc9c378bae139111efa7f3db5bc62feb3c649a
halimocakli <halimcanocakli@gmail.com>
2021-09-24 16:46:00 +0300

    Create Musteri.cs

---
+++ NetFramework.S17.D1.EnumNedir/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S17.D1.EnumNedir
+{
+    public class Musteri
+    {
+        public int musteriID { get; set; }
+        public string musteriIsim { get; set; }
+        public string musteriSoyIsim { get; set; }
+        public string musteriEmail { get; set; }
+        public string musteriNumara { get; set; }
+    }
+}
