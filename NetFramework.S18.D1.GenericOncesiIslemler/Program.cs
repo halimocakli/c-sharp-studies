1c50270dc380c92c1a475a4d7b61d7e8773c0de3
H. Can Ocaklı <62395643+halimocakli@users.noreply.github.com>
2021-11-19 00:16:21 +0300

    Create Program.cs

---
+++ NetFramework.S18.D1.GenericOncesiIslemler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D1.GenericOncesiIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SanalDatabase virtualDataBase = new SanalDatabase();
+            virtualDataBase.yeniKayit(1);
+            virtualDataBase.yeniKayit(2);
+
+            /// Bu kısımda, generic mimari yokken nasıl veri kaydı aldığımızı vs konuştuk
+        }
+    }
+}
