5b19aaa673bd6f5eb04326f664bb14c5838cc66f
H. Can Ocaklı <62395643+halimocakli@users.noreply.github.com>
2021-11-19 00:16:25 +0300

    Create SanalDatabase.cs

---
+++ NetFramework.S18.D1.GenericOncesiIslemler/SanalDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D1.GenericOncesiIslemler
+{
+    public class SanalDatabase
+    {
+        private ArrayList listem;
+
+        public SanalDatabase()
+        {
+            listem = new ArrayList();
+        }
+
+        public void yeniKayit(int data)
+        {
+            listem.Add(data);
+        }
+    }
+}
