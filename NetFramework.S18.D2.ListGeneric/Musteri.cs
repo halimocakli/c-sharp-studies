b2f6e325e0a5db14fe678925dc5358a648ddb0d5
H. Can Ocaklı <62395643+halimocakli@users.noreply.github.com>
2021-11-20 00:50:41 +0300

    Create Musteri.cs

---
+++ NetFramework.S18.D2.ListGeneric/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D2.ListGeneric
+{
+    class Musteri
+    {
+        public int ID { get; set; }
+        public string musteriIsim { get; set; }
+        public string musteriSoyIsim { get; set; }
+
+    }
+}
