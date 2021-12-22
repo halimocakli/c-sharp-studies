b7bcc53d236441bdc186ad3e04f5ec363648b1c7
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-12-23 01:41:36 +0300

    Create MusteriGenericClass.cs

---
+++ NetFramework.S18.D6.GenericClassNedir/MusteriGenericClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D6.GenericClassNeDegildir
+{
+    public class MusteriGenericClass<T> // <T> standart kalıpır ve opsiyoneldir
+    {
+        public T musteriID { get; set; }
+        public T musteriNumara { get; set; }
+        public string musteriTcKimlikNumarasi { get; set; }
+        public string musteriIsim { get; set; }
+        public string musteriSoyIsim { get; set; }
+        public DateTime musteriDogumTarihi { get; set; }
+
+        public T musteriNumarasiAl()
+        {
+            return musteriNumara;
+        }
+    }
+}
