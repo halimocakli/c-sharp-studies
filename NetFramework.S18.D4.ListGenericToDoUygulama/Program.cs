a799dc683bdedd786695d9859d6bef753d099d07
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-12-23 01:41:09 +0300

    Create Program.cs

---
+++ NetFramework.S18.D4.ListGenericToDoUygulama/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D4.ListGenericToDoUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Video ders içerisindeki uygulama alanı kısmında bulunan List<T> Uygulama - Windows Form dersi
+        }
+    }
+}
