001cd9d21faec9042fc4d7befa118a872efa0feb
Halim Can Ocaklı <halimcanocakli@gmail.com>
2021-12-23 01:41:55 +0300

    Create Urun.cs

---
+++ NetFramework.S18.D7.GenericClassNedir/Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S18.D7.GenericClassNedir
+{
+    public class Urun
+    {
+        public int ID { get; set; }
+        public string tanim { get; set; }
+    }
+}
