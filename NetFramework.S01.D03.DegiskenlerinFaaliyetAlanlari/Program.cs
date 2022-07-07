ec58d1dff2bf9b7feeae8c6224772ef7da8e4e7c
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-07-07 20:56:11 +0300

    Create Program.cs

---
+++ NetFramework.S01.D03.DegiskenlerinFaaliyetAlanlari/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S01.D03.DegiskenlerinFaaliyetAlanlari
+{
+    class Program
+    {
+        static string S2 = "Sabri Ayberk Hocaoğlu";
+        static void Main(string[] args)
+        {
+            // Oluşturduğumuz değişkeni kullanabileceğimiz belirli sınırlar vardır.
+            // Her bir değişken içerisinde oluşturulduğu süslü parantezler dahilinde geçerlidir.
+
+            string S1 = "Halim Can Ocaklı";
+            S1 = "Second Name";
+        }
+        static void Test()
+        {
+            // Bu kısımdan S1 değişkenine ulaşmak mümkün değildir.
+            // Herhangi bir değişkene kodun her noktasından ulaşabilmek için o değişkenin globalde tanımlı olması gerekir.
+            // Örneğin "S2" global bir değişkendir.
+
+            S2 = "Oğuzhan Deniz";
+        }
+    }
+}
