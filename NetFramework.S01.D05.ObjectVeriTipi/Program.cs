699de5c3d760f78034890beb3977394bb1e69b13
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-07-13 14:54:08 +0300

    Create Program.cs

---
+++ NetFramework.S01.D05.ObjectVeriTipi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S01.D05.ObjectVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object veri tipi, C# dili içerisindeki bütün veri yapılarının atasıdır.
+            // Integer, Decimal, String, Bool, Float, Double, Custom Classes
+            // Object veri tipi, diğer veri tiplerinin değer aralıklarından daha geniş bir değer aralığına sahiptir.
+
+            object O1 = 100;
+            object O2 = 10.90F;
+        }
+    }
+}
