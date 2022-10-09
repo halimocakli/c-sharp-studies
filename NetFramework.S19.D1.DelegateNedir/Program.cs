43b36f26b30d4aaf5c0200f406480c34ccd61cc0
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-10-09 13:01:03 +0300

    Update Program.cs

--- NetFramework.S19.D1.DelegateNedir/Program.cs
+++ NetFramework.S19.D1.DelegateNedir/Program.cs
@@ -6,9 +6,29 @@ using System.Threading.Tasks;
 
 namespace NetFramework.S19.D1.DelegateNedir
 {
+    
+    /*
+    * Delegate => Metot Tutucu olarak da isimlendirilir.
+    * Aynı metot imzasına sahip metotları tutan ve onları bir liste olarak kendi içerisinde barındıran yapıdır.
+    * Metot imzası, bir metodun aldığı parametreler, bu parametrelerin veri türü ve geri döndürdüğü değerin veri türüdür.
+    * Yani biz birden fazla metodu aynı anda çağırmak yerine bir delegate üzerinden çağırıp metotları sırasıyla çağırabiliriz.
+    * "Linq" sorgularında sıklıkla kullanılır.
+    * Her delegenin base sınıfı "Delegate" sınıfıdır.
+    * Kısacası delegate'ler bir metot silsilesidir, metotları işaret eder ve çalıştığında bir veya birden fazla metodu tetikler.
+    * Metotlar, delegate'nin içine eklenen sıra ile çalışır.
+    *
+    * ==> Delegate int MatematikselIslemler(int A, int B) <==
+    *
+    * Yukarıdaki prototipte görüldüğü gibi, iki adet int parametre alan ve geriye int değer döndüren metotlarımızı bir delegate içerisinde tutabiliriz.
+    *
+    * Diyelim ki bu örnek delegete içerisine "Topla", "Çarp", "Çıkart" metotlarımızı belli bir sıra ile ekledik. İlgili delegate çağırıldığı zaman,
+    * eklenme sırasına göre metotlarımız çalışacaktır.
+    */
+    
     class Program
     {
         delegate void MatematikselIslemler(int Sayi1, int Sayi2);
+
         static void Topla(int sayi1, int sayi2)
         {
             int sonuc = sayi1 + sayi2;
@@ -27,23 +47,6 @@ namespace NetFramework.S19.D1.DelegateNedir
 
         static void Main(string[] args)
         {
-            /*
-             * Delegate => Metot Tutucu olarak da isimlendirilir.
-             * Aynı metot imzasına sahip metotları tutan ve onları bir liste olarak kendi içerisinde barındıran yapıdır.
-             * Metot imzası, bir metodun aldığı parametrelerin ve geri döndürdüğü değerin veri türüdür.
-             * Yani biz birden fazla metodu aynı anda çağırmak yerine bir delegate üzerinden çağırıp metotları sırasıyla çağırabiliriz.
-             * Linq sorgularında sıklıkla kullanılır.
-             * Her delegenin base sınıfı "Delegate" sınıfıdır.
-             * Kısacası delegate'ler bir metot silsilesidir ve çalıştığında bir veya birden fazla metodu tetikler.
-             *
-             * ==> Delegate int MatematikselIslemler(int A, int B) <==
-             *
-             * Yukarıdaki prototipte görüldüğü gibi, iki adet int parametre alan ve geriye int değer döndüren metotlarımızı bir delegate içerisinde tutabiliriz.
-             *
-             * Diyelim ki bu örnek delegete içerisine "Topla", "Çarp", "Çıkart" metotlarımızı belli bir sıra ile ekledik. İlgili delegate çağırıldığı zaman,
-             * eklenme sırasına göre metotlarımız çalışacaktır.
-             */
-
             Topla(10, 20);
             Cikart(20, 2);
             Carp(20, 2);
@@ -52,7 +55,8 @@ namespace NetFramework.S19.D1.DelegateNedir
 
             // En standart haliyle üç adet static metot oluşturdum ve bunları kullandım, peki bu metotları bir delege içerisinde nasıl kullanabilirim?
 
-            MatematikselIslemler M1 = new MatematikselIslemler(Topla);  // MatematikselIslemler delegemizi örnekledik, yarattığımız nesnenin Topla metodunu işaret etmesini sağladık.
+            // MatematikselIslemler delegemizi örnekledik, yarattığımız nesnenin Topla metodunu işaret etmesini sağladık.
+            MatematikselIslemler M1 = new MatematikselIslemler(Topla);  
             M1.Invoke(20, 30);    // Invoke() metodu, ilgili delegeyi çağırmamızı sağlar.
 
             Console.WriteLine("\nMatematikselIslemler delegesi içerisindeki Topla() metodu çalıştı\n");
